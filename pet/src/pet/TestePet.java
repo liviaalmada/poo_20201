1b028c3ddae561e62abde28bfaaed55fcb09d203
Livia <Livia@DESKTOP-HQFJ1NF>
2020-07-21 18:56:48 -0300

    Adicionando arquivo com exemplo de uso.

---
+++ pet/src/pet/TestePet.java
@@ -0,0 +1,26 @@
+package pet;
+
+public class TestePet {
+	
+	public static void main(String[] args) {
+		Pet tamagoshi = new Pet(3, 3, 3);
+		tamagoshi.show();
+			
+		tamagoshi.shower();
+		tamagoshi.show();
+		
+		tamagoshi.play();
+		tamagoshi.show();
+		
+		tamagoshi.eat();
+		tamagoshi.show();
+		
+		tamagoshi.sleep();
+		tamagoshi.show();	
+		
+		tamagoshi.play();
+		tamagoshi.show();
+		
+	}
+
+}
