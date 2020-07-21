b08c1b1ea3a29958df175d10ef68fed971e90e5b
Livia <Livia@DESKTOP-HQFJ1NF>
2020-07-21 19:30:32 -0300

    Atualização

--- pet/src/pet/TestePet.java
+++ pet/src/pet/TestePet.java
@@ -5,21 +5,87 @@ public class TestePet {
 	public static void main(String[] args) {
 		Pet tamagoshi = new Pet(3, 3, 3);
 		tamagoshi.show();
-			
-		tamagoshi.shower();
-		tamagoshi.show();
-		
+		/* Saída esperada: 
+		diamonds = 0
+		energy = 3
+		hungry = 0
+		clean = 3
+		age = 0 
+		*/
+					
 		tamagoshi.play();
-		tamagoshi.show();
+		// Saída esperada: 
+		// Seu pet está brincando!
+		
+		tamagoshi.show();		
+		/* Saída esperada: 
+		diamonds = 1
+		energy = 2
+		hungry = 1
+		clean = 2
+		age = 1 
+		*/
+		
+		tamagoshi.shower();
+		// Saída esperada: 
+		// Seu pet está tomando banho!
+		
+		tamagoshi.show();		
+		/* Saída esperada: 
+		diamonds = 1
+		energy = 1
+		hungry = 2
+		clean = 3
+		age = 2 
+		*/
 		
 		tamagoshi.eat();
-		tamagoshi.show();
+		// Saída esperada: 
+		// Seu pet está comendo!
 		
-		tamagoshi.sleep();
-		tamagoshi.show();	
+		tamagoshi.show();
+		/* Saída esperada: 
+		diamonds = 1
+		energy = 2
+		hungry = 1
+		clean = 2
+		age = 3
+		*/
+
+		tamagoshi.eat();
+		// Saída esperada: 
+		// Seu pet está comendo!	
+		
+		tamagoshi.shower();
+		// Saída esperada: 
+		// Seu pet está tomando banho!
 		
 		tamagoshi.play();
-		tamagoshi.show();
+		// Saída esperada: 
+		// Seu pet está brincando!
+		
+		tamagoshi.show();		
+		/* Saída esperada: 
+		diamonds = 2
+		energy = 1
+		hungry = 2
+		clean = 1
+		age = 6
+		*/
+			
+		tamagoshi.play();
+		// Seu pet está brincando!
+		// Seu pet morreu sem energia!
+		// Seu pet morreu sujo!
+		
+		tamagoshi.show();		
+		/* Saída esperada: 
+		diamonds = 3
+		energy = 0
+		hungry = 3
+		clean = 0
+		age = 6
+		*/
 		
 	}
 
